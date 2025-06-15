a4c4a45d51e5305ee71c39c965ef5d72d71d5032
Inoriant <424504326@qq.com>
2025-06-15 16:42:40 +0800

    init

--- include/nvrhi/common/resource.h
+++ include/nvrhi/common/resource.h
@@ -19,7 +19,7 @@
 * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 * DEALINGS IN THE SOFTWARE.
 */
-
+#pragma once
 #include <atomic>
 #include <cstdint>
 #include <type_traits>
